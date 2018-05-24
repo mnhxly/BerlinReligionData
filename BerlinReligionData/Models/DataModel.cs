2e708e17171eb7187439e72a0d6dfc0212992007
Malte <malte.hoenicke@gmail.com>
2018-05-24 22:14:22 +0200

    save

---
+++ BerlinReligionData/Models/DataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LeenartMalteProject.Models
+{
+    public class Religion
+    {
+
+        public string ReligionType { get; set; }
+        public int Year { get; set; }
+        public int Participant { get; set; }
+        public int BudgetYear {get; set;}
+        public int Subvention { get; set; }
+
+
+        public DataModel()
+        {
+        }
+    }
+}
