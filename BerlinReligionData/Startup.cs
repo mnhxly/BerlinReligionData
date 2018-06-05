e17b16b9a8722ab82f8a83285b52a89b9f0b1340
Unknown <malte.hoenicke@gmail.com>
2018-06-05 22:45:15 +0200

    Save

--- BerlinReligionData/Startup.cs
+++ BerlinReligionData/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace LeenartMalteProject
+namespace BerlinReligionData
 {
     public class Startup
     {
