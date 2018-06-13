4096dee148aa11d9008306b54629d2b2f6fa88c1
Lennart Binscheck <lbinscheck@web.de>
2018-06-13 10:57:00 +0200

    'frontend'

--- BerlinReligionData/Controllers/HomeController.cs
+++ BerlinReligionData/Controllers/HomeController.cs
@@ -6,23 +6,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BerlinReligionData.Models;
 using BerlinReligionData.DAL;
+using Newtonsoft.Json;
 
 namespace BerlinReligionData.Controllers
 {
 
-   
-
-
     public class HomeController : Controller
     {
 
-        private readonly mysqlContext context;
-        public HomeController(mysqlContext context) {
-            this.context = context;
-        }
+        //private readonly mysqlContext context;
+        ////public HomeController(mysqlContext context) {
+        ////    this.context = context;
+        ////}
 
-        public IActionResult Index()
+        public ActionResult Index()
         {
+            List<DataPoint> dataPoints = new List<DataPoint>();
+            List<DataPoint> dataPoints1 = new List<DataPoint>();
+            
+            dataPoints.Add(new DataPoint(1496341800000, 2790));
+            dataPoints.Add(new DataPoint(1496428200000, 3380));
+
+            dataPoints1.Add(new DataPoint(1496341800000, 4000));
+            dataPoints1.Add(new DataPoint(1496428200000, 6000));
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+            ViewBag.DataPoints1 = JsonConvert.SerializeObject(dataPoints1);
+
             return View();
         }
 
