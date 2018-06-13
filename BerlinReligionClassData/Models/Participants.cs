99268b012ab8fef01aac38476ef7906b498d7d3f
Lennart <binscheck@sport-thieme.com>
2018-06-13 17:06:32 +0200

    new start

---
+++ BerlinReligionClassData/Models/Participants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BerlinReligionClassData.Models
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public double ParticipantAmount { get; set; }
+        public int Year { get; set; }
+        public string Religion { get; set; }
+
+        public Participant(int id, double participantAmount, int year, string religion)
+        {
+            this.Id = id;
+            this.ParticipantAmount = participantAmount;
+            this.Year = year;
+            this.Religion = religion;
+        }
+
+    }
+}
---
+++ BerlinReligionClassData/Models/Subvention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BerlinReligionClassData.Models
+{
+    public class Subvention
+    {
+        [Key]
+        public int Id { get; set; }
+        public double SubventionAmount { get; set; }
+        public int Year { get; set; }
+        public string Religion { get; set; }
+
+        public Subvention(int id, double subventionAmount, int year, string religion)
+        {
+            this.Id = id;
+            this.SubventionAmount = subventionAmount;
+            this.Year = year;
+            this.Religion = religion;
+        }
+
+    }
+}
