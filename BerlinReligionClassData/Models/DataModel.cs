9f2ac5009671120baad48c522e9b261b20735b17
Lennart <binscheck@sport-thieme.com>
2018-06-18 19:51:39 +0200

    frontend und daten im model

--- BerlinReligionClassData/Models/DataModel.cs
+++ BerlinReligionClassData/Models/DataModel.cs
@@ -22,7 +22,6 @@ namespace BerlinReligionClassData.Models {
             using (var context = new ReligionDatabaseContext ()) {
                 var participants = from v in context.Paricipants where v.Religion == religion select v;
 
-<<<<<<< Updated upstream
                 foreach (var participant in participants) {
                     dataPoints.Add (new DataPoint (Convert.ToString (participant.Year), participant.ParticipantAmount));
                 }
@@ -30,23 +29,8 @@ namespace BerlinReligionClassData.Models {
 
             return JsonConvert.SerializeObject (dataPoints);
         }
-=======
-                var sub1 = new Subvention(1, 26170923, 2011, "Evangelsiche Kische", 1);
-                var sub2 = new Subvention(2, 25927285, 2012, "Evangelsiche Kische", 1);
-                var sub3 = new Subvention(3, 25601313, 2013, "Evangelsiche Kische", 1);
-                var sub4 = new Subvention(4, 25722940, 2014, "Evangelsiche Kische", 1);
-                var sub5 = new Subvention(5, 25688712, 2015, "Evangelsiche Kische", 1);
-                var sub6 = new Subvention(6, 28134983, 2016, "Evangelsiche Kische", 1);
 
-                var sub7 = new Subvention(7, 13603080, 2011, "Humanistischer Verband",4);
-                var sub8 = new Subvention(8, 14160080, 2012, "Humanistischer Verband", 4);
-                var sub9 = new Subvention(9, 14645942, 2013, "Humanistischer Verband", 4);
-                var sub10 = new Subvention(10, 15110188, 2014, "Humanistischer Verband", 4);
-                var sub11 = new Subvention(11, 15574620, 2015, "Humanistischer Verband", 4);
-                var sub12 = new Subvention(12, 18082480, 2016, "Humanistischer Verband", 4);
->>>>>>> Stashed changes
-
-        public string FilterParticipantsByYear(int year, string religion){
+        public string FilterParticipantsByYear (int year, string religion) {
             List<DataPoint> dataPoints = new List<DataPoint> ();
 
             using (var context = new ReligionDatabaseContext ()) {
@@ -62,16 +46,7 @@ namespace BerlinReligionClassData.Models {
 
         public string FilterDataByYear (int year = 2011) {
 
-<<<<<<< Updated upstream
             List<DataPoint> dataPoints = new List<DataPoint> ();
-=======
-                var par1 = new Participant(1, 80393, 2011, "Evangelischer Religionsunterricht", 1);
-                var par2 = new Participant(2, 78800, 2012, "Evangelischer Religionsunterricht", 1);
-                var par3 = new Participant(3, 78255, 2013, "Evangelischer Religionsunterricht", 1);
-                var par4 = new Participant(4, 78735, 2014, "Evangelischer Religionsunterricht", 1);
-                var par5 = new Participant(5, 78771, 2015, "Evangelischer Religionsunterricht", 1);
-                var par6 = new Participant(6, 79549, 2016, "Evangelischer Religionsunterricht", 1);
->>>>>>> Stashed changes
 
             using (var context = new ReligionDatabaseContext ()) {
                 var subventionYears = from v in context.Subventions where v.Year == year select v;
